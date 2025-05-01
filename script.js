9afe515cb70897007069cedbe6fc75b2ba9988e0
Abhilash Kumar Mehta <145607994+ABHILASH-KUMAR-MEHTA@users.noreply.github.com>
2025-05-01 23:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const statusParagraph = document.getElementById("status");
 const enterButton = document.getElementById("enterBtn");
 
 enterButton.addEventListener("click", () => {
-  const h1 = document.createElement("h1"); // Create new <h1>
-  h1.textContent = "Entered Metaverse"; // Set its content
-  statusParagraph.replaceWith(h1); // Replace <p> with <h1>
+  statusParagraph.textContent = "Entered Metaverse";
+  statusParagraph.classList.add("updated");
 });
